476ea2dfad974ed1f73b357e55b25dec36ed1eb5
lgy112112 <144128974+lgy112112@users.noreply.github.com>
2024-01-15 00:55:32 +0800

    Update train.py

--- train.py
+++ train.py
@@ -11,26 +11,26 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 import torch.nn as nn
 
-# 设备配置（使用 GPU 或 CPU）
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# 创建模型实例
+
 model = UNet().to(device)
 
-# 定义损失函数和优化器
+
 criterion = nn.MSELoss()
 
 optimizer = optim.AdamW(model.parameters(), lr=0.0001)
-# 创建感知损失函数实例
+
 perceptual_loss = PerceptualLoss().to(device)
-# 准备数据集
+
 train_dataset = ImageDataset(noisy_dir='noisy_images', clean_dir='clean_images')
 train_loader = DataLoader(dataset=train_dataset, batch_size=16, shuffle=True)
 
 # 训练模型
-num_epochs = 30  # 您可以调整这个值
+num_epochs = 30  
 for epoch in range(num_epochs):
-    # 使用 tqdm 包装训练数据加载器
+    
     with tqdm(enumerate(train_loader), total=len(train_loader), desc=f"Epoch {epoch + 1}/{num_epochs}", unit="batch") as tepoch:
         for i, (noisy_imgs, clean_imgs) in tepoch:
 
