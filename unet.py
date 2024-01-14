a7e73bb79685b36934768ee0f632e6ae260e2626
lgy112112 <144128974+lgy112112@users.noreply.github.com>
2024-01-14 11:14:39 +0800

    ok

--- unet.py
+++ unet.py
@@ -103,7 +103,7 @@ class UNetUpBlock(nn.Module):
             self.up = nn.ConvTranspose2d(in_size, out_size, kernel_size=2, stride=2)
         elif up_mode == 'upsample':
             self.up = nn.Sequential(
-                nn.Upsample(mode='trlinear', scale_factor=2),
+                nn.Upsample(mode='bilinear', scale_factor=2),
                 nn.Conv2d(in_size, out_size, kernel_size=1),
             )
 
